23c7f730acd4ef53f7c2520c364636ab102d8365
cemrezr <cemrezereyak@gmail.com>
2019-08-01 11:06:02 +0300

    added entitiy

--- src/main/java/com/test/pluto/HelloController.java
+++ src/main/java/com/test/pluto/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 @RequestMapping (method=RequestMethod.GET)
     public String printHello(ModelMap model){
-    model.addAttribute("message", "Hello Spring MVC Framework!");
+    model.addAttribute("message", "Hello Spring MVC Framework ");
     return "hello";
 }
 }
