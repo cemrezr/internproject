03a6008bb7423476f3dd8473dc23a5aff44493eb
cemrezr <cemrezereyak@gmail.com>
2019-08-05 13:47:49 +0300

    addedn entities

---
+++ src/main/java/com/test/pluto/entities/PhoneEntity.java
@@ -0,0 +1,50 @@
+package com.test.pluto.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "phone", schema = "income", catalog = "")
+public class PhoneEntity {
+    private int id;
+    private int number;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "number")
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PhoneEntity that = (PhoneEntity) o;
+
+        if (id != that.id) return false;
+        if (number != that.number) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + number;
+        return result;
+    }
+}
---
+++ src/main/java/com/test/pluto/entities/ServiceEntity.java
@@ -0,0 +1,63 @@
+package com.test.pluto.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "service", schema = "income", catalog = "")
+public class ServiceEntity {
+    private int id;
+    private String name;
+    private int price;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "price")
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ServiceEntity that = (ServiceEntity) o;
+
+        if (id != that.id) return false;
+        if (price != that.price) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + price;
+        return result;
+    }
+}
